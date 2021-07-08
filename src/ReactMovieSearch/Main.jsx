02d82e49b863bf9a3b2edc0e12f0092eb1d89ac5
Niku_419 <2019198@iiitdmj.ac.in>
2021-07-08 13:07:00 +0530

    :monocle_face: some unknown changes in Main.jsx

--- src/ReactMovieSearch/Main.jsx
+++ src/ReactMovieSearch/Main.jsx
@@ -1,41 +1,64 @@
 import React from 'react'
-import { Container, Badge } from 'react-bootstrap'
+// import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import { faInstagram, faGithub, faFacebookF, faLinkedinIn} from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Container, Badge, Nav, Navbar } from 'react-bootstrap'
 import TitleSearch from './TitleSearch'
 import CustomSearch from './CustomSearch'
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 
 export default function Main() {
   return (
-    <Container className="pt-5 justified-container-center">
-      <Parallax pages={2} style={{ top: '0', left: '0' }}>
-        <ParallaxLayer
-          offset={0}
-          speed={2.5} 
-          className="row"
-          style={{ 
-            display: 'flex', 
-            justifyContent: 'center', 
-            alignItems: 'center',
-            backgroundColor: '#66fcf1'
-          }}>
-          <CustomSearch className="col"/>
-          <h4 className="pt-5 text-center col d-flex flex-row-reverse">
-            <Badge variant="dark">Scroll down to search by title</Badge>
-          </h4>
-        </ParallaxLayer>
-        <ParallaxLayer offset={1} speed={2} style={{ backgroundColor: '#fc4445' }} />
-        <ParallaxLayer
-          offset={1}
-          speed={0.5}
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            color: 'black',
-          }}>
-          <TitleSearch/>
-        </ParallaxLayer>
-      </Parallax>
-    </Container>
+    <>
+      <Container className="pt-5 justified-container-center">
+        <Parallax pages={2} style={{ top: '0', left: '0' }}>
+          <ParallaxLayer
+            offset={0}
+            speed={2.5} 
+            className="row"
+            style={{ 
+              display: 'flex', 
+              justifyContent: 'center', 
+              alignItems: 'center',
+              backgroundColor: '#66fcf1'
+            }}>
+            <CustomSearch className="col"/>
+            <h4 className="pt-5 text-center col d-flex flex-row-reverse">
+              <Badge variant="dark">Scroll down to search by title</Badge>
+            </h4>
+          </ParallaxLayer>
+          <ParallaxLayer offset={1} speed={2} style={{ backgroundColor: '#fc4445' }} />
+          <ParallaxLayer
+            offset={1}
+            speed={0.5}
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              color: 'black',
+            }}>
+            <TitleSearch/>
+          </ParallaxLayer>
+        </Parallax>
+      </Container>
+      <Container>
+        <Navbar bg="transparent" fixed="bottom" variant="light">
+          <Navbar.Brand >
+            <strong>Niku419</strong>
+          </Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="https://github.com/niku419"><FontAwesomeIcon  icon={faGithub} /></Nav.Link>
+            <Nav.Link href="https://linkedin.com/niku_419"><FontAwesomeIcon  icon={faLinkedinIn} /></Nav.Link>
+            <Nav.Link href="https://instagram.com/_niku_419"><FontAwesomeIcon  icon={faInstagram} /></Nav.Link>
+            <Nav.Link href="#"><FontAwesomeIcon icon={faFacebookF} /></Nav.Link>
+          </Nav>
+          <Form inline>
+            {/* <Nav className="mr-auto"> 
+              <Nav.Link>made for Hiku<FontAwesomeIcon color="#8d0101" icon={faHeart} /></Nav.Link>
+            </Nav> */}
+          </Form>
+        </Navbar>
+      </Container>
+    </>
   )
 }
